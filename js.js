9b5ee14e59d266b2da6703b4d50bed1cb77fbdb3
leonshih <leonshih725@gmail.com>
2016-01-15 15:27:09 +0800

    .

--- js.js
+++ js.js
@@ -1,15 +1,22 @@
-var key_token = 'AIzaSyAp_CtjwR0XSK12HNigongdmD8RGJHI0CU';
+var key_token = 'AIzaSyBzMomwt4w-woNKe0UlPJgZ14k1OEeEYO8';
 
 $(function(){
-	$.get('https://www.googleapis.com/blogger/v3/blogs/7209041933557286912/posts?key=' + key_token, function(data){
+	$.get('https://www.googleapis.com/blogger/v3/blogs/5768039957092517741/posts?key=' + key_token, function(data){
 		for(i=0; i<data.items.length; i++){
 			$('#post_titles').append($('<option></option>').attr('value', data.items[i].id).text(data.items[i].title));
 		}
 	});
-	
+	$.post('https://www.googleapis.com/blogger/v3/blogs/5768039957092517741/posts', {
+		'kind': 'blogger#post',
+		'blog': {
+			'id': '5768039957092517741'
+		},
+		'title': 'new post',
+		'content': 'a new content'
+	});
 	$('#post_titles').on('change', function(){
 		var postId = $('#post_titles').val();
-		$.get('https://www.googleapis.com/blogger/v3/blogs/7209041933557286912/posts/' + postId + '?key=' + key_token, function(data){
+		$.get('https://www.googleapis.com/blogger/v3/blogs/5768039957092517741/posts/' + postId + '?key=' + key_token, function(data){
 			var content = data.content;
 			$('#showArea')[0].innerHTML = content;
 			$('#txtArea')[0].innerHTML =  $('#showArea')[0].innerHTML.replace(/<br.*?>/g, '\n');
